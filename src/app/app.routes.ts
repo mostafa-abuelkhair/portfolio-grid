515b8fbe83e593687e327e60b019eabd72879749
mostafa abu elkhair <mostafa.ahmed2509@gmail.com>
2023-12-09 03:53:38 +0200

    starting

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent),title:'Home'},
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavComponent,FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
